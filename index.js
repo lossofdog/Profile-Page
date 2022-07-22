da2abe5e404a40d1114ecf762a23d536cc975d8b
Ian <iantwelve@gmail.com>
2022-07-21 20:59:17 -0700

    followed along to update the js to match html

--- index.js
+++ index.js
@@ -13,10 +13,32 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
+
+let description = document.createElement('h3')
+description.append('Description:')
+dogDetails.append(description)
+
+let p = document.createElement('p')
+p.textContent = 'This is some text placeholder'
+dogDetails.append(p)
+
+let desc2 = document.createElement('h3')
+desc2.append('Feeding Times:')
+dogDetails.append(desc2)
+
+let ul = document.createElement('ul')
+let times = ['9:00am','12:00pm']
+for (let i = 0; i<times.length;i++){
+    li = document.createElement('li')
+    li.append(times[i])
+    ul.append(li)
+}
+
+dogDetails.append(ul)
 
 
